c5688853fad6a2cae3605c3267ecb1e056612cfb
Naveen512 <bommidinaveen@gmail.com>
2016-02-04 21:01:07 +0530

    First SignalR EXample

---
+++ FirstSignalRExample/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace FirstSignalRExample
+{
+    public class ChatHub : Hub
+    {
+       
+        public void Send(string Name,string Message)
+        {
+            Clients.All.broadCastMessage(Name, Message);
+        }
+    }
+}
