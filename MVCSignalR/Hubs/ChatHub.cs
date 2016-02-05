12444358007bb70e39aa3df57efa703711cc5fbd
Naveen512 <bommidinaveen@gmail.com>
2016-02-05 10:07:03 +0530

    Added New Project 'MVCSignlR' to Existing Solution

---
+++ MVCSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MVCSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string Name,string Message)
+        {
+            Clients.All.addNewMessageToPage(Name, Message);
+        }
+    }
+}
